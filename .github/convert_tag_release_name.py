fa4aa722b1584bf8f46ea3d6be01a3d8176e9ecd
Artem Chepurnoy <artemchep@gmail.com>
2024-05-04 13:32:10 +0300

    fix: Correctly parse release tag as date #332

---
+++ .github/convert_tag_release_name.py
@@ -0,0 +1,18 @@
+import argparse
+import re
+
+from datetime import datetime
+
+parser = argparse.ArgumentParser("convert_tag_release_name")
+parser.add_argument("tag", help="Release tag", type=str)
+args = parser.parse_args()
+
+tag = args.tag
+# A format of the tag is r+yyyyMMdd, we
+# strip out any non digit symbols here.
+date_str = re.sub(r'[^\d]', '', tag)
+date = datetime.strptime(date_str, '%Y%m%d')
+
+# Release date name.
+out = "{:%Y.%m.%d}".format(date)
+print(out)

fdb5d8dfb328895c9c6ca1c7f782202d93ec1f6f
Artem Chepurnyi <artemchep@gmail.com>
2024-12-10 10:23:16 +0200

    fix: Fix compatibility issue with richtext 1.0.0-alpha02

--- common/src/commonMain/kotlin/com/artemchep/keyguard/ui/markdown/Markdown.kt
+++ common/src/commonMain/kotlin/com/artemchep/keyguard/ui/markdown/Markdown.kt
@@ -2,11 +2,14 @@ package com.artemchep.keyguard.ui.markdown
 
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.platform.UriHandler
+import androidx.compose.ui.text.TextLinkStyles
 import com.artemchep.keyguard.common.model.ToastMessage
 import com.artemchep.keyguard.common.usecase.ShowMessage
 import com.artemchep.keyguard.feature.localization.textResource
@@ -16,7 +19,6 @@ import com.artemchep.keyguard.res.*
 import com.halilibo.richtext.commonmark.Markdown
 import com.halilibo.richtext.markdown.BasicMarkdown
 import com.halilibo.richtext.markdown.node.AstNode
-import com.halilibo.richtext.ui.LinkClickHandler
 import com.halilibo.richtext.ui.RichTextStyle
 import com.halilibo.richtext.ui.material3.RichText
 import com.halilibo.richtext.ui.resolveDefaults
@@ -30,13 +32,14 @@ fun MarkdownText(
     modifier: Modifier = Modifier,
     markdown: String,
 ) {
-    val richTextStyle = getRichTextStyle()
-    RichText(
-        modifier = modifier,
-        style = richTextStyle,
-        linkClickHandler = rememberGracefulLinkClickHandler(),
-    ) {
-        Markdown(markdown)
+    ProvideGracefulUriHandler {
+        val richTextStyle = getRichTextStyle()
+        RichText(
+            modifier = modifier,
+            style = richTextStyle,
+        ) {
+            Markdown(markdown)
+        }
     }
 }
 
@@ -45,47 +48,59 @@ fun MarkdownText(
     modifier: Modifier = Modifier,
     markdown: AstNode,
 ) {
-    val richTextStyle = getRichTextStyle()
-    RichText(
-        modifier = modifier,
-        style = richTextStyle,
-        linkClickHandler = rememberGracefulLinkClickHandler(),
-    ) {
-        BasicMarkdown(markdown)
+    ProvideGracefulUriHandler {
+        val richTextStyle = getRichTextStyle()
+        RichText(
+            modifier = modifier,
+            style = richTextStyle,
+        ) {
+            BasicMarkdown(markdown)
+        }
     }
 }
 
 @Composable
-private fun rememberGracefulLinkClickHandler(): LinkClickHandler {
+private fun ProvideGracefulUriHandler(
+    content: @Composable () -> Unit,
+) {
     val showMessage by rememberInstance<ShowMessage>()
 
     val updatedContext by rememberUpdatedState(LocalLeContext)
     val updatedUriHandler by rememberUpdatedState(LocalUriHandler.current)
-    return remember {
-        LinkClickHandler { uri ->
-            try {
-                updatedUriHandler.openUri(uri)
-            } catch (e: Exception) {
-                GlobalScope.launch {
-                    val title = textResource(Res.string.error_failed_open_uri, updatedContext)
-                    val msg = ToastMessage(
-                        title = title,
-                        text = e.message,
-                        type = ToastMessage.Type.ERROR,
-                    )
-                    showMessage.copy(msg)
+
+    val uriHandler = remember {
+        object : UriHandler {
+            override fun openUri(uri: String) {
+                try {
+                    updatedUriHandler.openUri(uri)
+                } catch (e: Exception) {
+                    GlobalScope.launch {
+                        val title = textResource(Res.string.error_failed_open_uri, updatedContext)
+                        val msg = ToastMessage(
+                            title = title,
+                            text = e.message,
+                            type = ToastMessage.Type.ERROR,
+                        )
+                        showMessage.copy(msg)
+                    }
                 }
             }
         }
+    }
+
+    return CompositionLocalProvider(LocalUriHandler provides uriHandler) {
+        content()
     }
 }
 
 @Composable
 private fun getRichTextStyle(): RichTextStyle {
     return RichTextStyle().resolveDefaults().copy(
-        stringStyle = RichTextStringStyle().copy(
-            linkStyle = MaterialTheme.typography.bodyLarge.toSpanStyle().copy(
-                color = MaterialTheme.colorScheme.primary,
+        stringStyle = RichTextStringStyle(
+            linkStyle = TextLinkStyles(
+                style = MaterialTheme.typography.bodyLarge.toSpanStyle().copy(
+                    color = MaterialTheme.colorScheme.primary,
+                ),
             ),
         ),
     )

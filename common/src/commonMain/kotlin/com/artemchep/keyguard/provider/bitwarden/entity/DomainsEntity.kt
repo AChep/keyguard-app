d03a2ad16cbac39c1a70d1522918f8607fd333d1
Artem Chepurnyi <artemchep@gmail.com>
2024-12-20 21:01:59 +0200

    fix(Sync): Specify default values for Domains entity #753

--- common/src/commonMain/kotlin/com/artemchep/keyguard/provider/bitwarden/entity/DomainsEntity.kt
+++ common/src/commonMain/kotlin/com/artemchep/keyguard/provider/bitwarden/entity/DomainsEntity.kt
@@ -2,11 +2,14 @@ package com.artemchep.keyguard.provider.bitwarden.entity
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonNames
 
 @Serializable
 data class DomainsEntity(
+    @JsonNames("GlobalEquivalentDomains")
     @SerialName("globalEquivalentDomains")
-    val globalEquivalentDomains: List<GlobalEquivalentDomainEntity>?,
+    val globalEquivalentDomains: List<GlobalEquivalentDomainEntity>? = null,
+    @JsonNames("EquivalentDomains")
     @SerialName("equivalentDomains")
-    val equivalentDomains: List<List<String>>?,
+    val equivalentDomains: List<List<String>>? = null,
 )
--- common/src/commonMain/kotlin/com/artemchep/keyguard/provider/bitwarden/entity/GlobalEquivalentDomainEntity.kt
+++ common/src/commonMain/kotlin/com/artemchep/keyguard/provider/bitwarden/entity/GlobalEquivalentDomainEntity.kt
@@ -2,13 +2,17 @@ package com.artemchep.keyguard.provider.bitwarden.entity;
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonNames
 
 @Serializable
 data class GlobalEquivalentDomainEntity(
+    @JsonNames("Excluded")
     @SerialName("excluded")
-    val excluded: Boolean,
+    val excluded: Boolean = false,
+    @JsonNames("Domains")
     @SerialName("domains")
-    val domains: List<String>?,
+    val domains: List<String>? = null,
+    @JsonNames("Type")
     @SerialName("type")
     val type: Int,
 )

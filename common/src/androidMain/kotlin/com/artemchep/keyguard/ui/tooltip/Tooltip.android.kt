4d6a9a7ec856d18c527ded29f49225094419e3f0
Artem Chepurnyi <artemchep@gmail.com>
2024-12-29 11:58:28 +0200

    fix: Add empty Tooltip implementation for Android platform

--- common/src/androidMain/kotlin/com/artemchep/keyguard/ui/tooltip/Tooltip.android.kt
+++ common/src/androidMain/kotlin/com/artemchep/keyguard/ui/tooltip/Tooltip.android.kt
@@ -1,13 +1,20 @@
 package com.artemchep.keyguard.ui.tooltip
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
-expect fun <T : Any> Tooltip(
-    modifier: Modifier = Modifier,
+actual fun <T : Any> Tooltip(
+    modifier: Modifier,
     valueOrNull: T?,
     tooltip: @Composable BoxScope.(T) -> Unit,
     content: @Composable () -> Unit,
-)
+) {
+    Box(
+        modifier = modifier,
+    ) {
+        content()
+    }
+}

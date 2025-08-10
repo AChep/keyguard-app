5c4133e4b54ba9f1794405126770965809f144d0
Artem Chepurnyi <artemchep@gmail.com>
2025-08-10 18:21:26 +0300

    improvement(M3): Expressify Filters component

--- common/src/commonMain/kotlin/com/artemchep/keyguard/feature/search/filter/component/FilterItem.kt
+++ common/src/commonMain/kotlin/com/artemchep/keyguard/feature/search/filter/component/FilterItem.kt
@@ -4,16 +4,22 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.LocalMinimumInteractiveComponentSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.ToggleButton
+import androidx.compose.material3.ToggleButtonDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
@@ -34,6 +40,7 @@ import com.artemchep.keyguard.ui.theme.combineAlpha
 import com.artemchep.keyguard.ui.theme.selectedContainer
 import com.artemchep.keyguard.ui.util.DividerColor
 
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun FilterItemComposable(
     modifier: Modifier = Modifier,
@@ -44,32 +51,73 @@ fun FilterItemComposable(
     onClick: (() -> Unit)?,
     enabled: Boolean = onClick != null,
 ) {
-    FilterItemLayout(
-        modifier = modifier,
-        checked = checked,
-        leading = leading,
-        content = {
-            Text(
-                title,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis,
-                style = MaterialTheme.typography.labelLarge,
-            )
-            val summaryOrNull = text?.takeUnless { it.isBlank() }
-            ExpandedIfNotEmpty(valueOrNull = summaryOrNull) { summary ->
-                Text(
-                    summary,
-                    maxLines = 2,
-                    overflow = TextOverflow.Ellipsis,
-                    style = MaterialTheme.typography.labelSmall,
-                    color = LocalContentColor.current
-                        .combineAlpha(alpha = MediumEmphasisAlpha),
+    val updatedOnClick by rememberUpdatedState(onClick)
+    CompositionLocalProvider(
+        LocalMinimumInteractiveComponentSize provides 0.dp,
+    ) {
+        ToggleButton(
+            modifier = modifier
+                .widthIn(
+                    min = 48.dp,
+                )
+                .heightIn(
+                    min = 32.dp,
+                ),
+            checked = checked,
+            enabled = enabled,
+            colors = ToggleButtonDefaults.toggleButtonColors()
+                .copy(
+                    containerColor = Color.Transparent,
+                    disabledContainerColor = Color.Transparent,
+                ),
+            contentPadding = PaddingValues(
+                horizontal = 12.dp,
+                vertical = 4.dp,
+            ),
+            border = run {
+                val borderColor = LocalContentColor.current
+                    .combineAlpha(DisabledEmphasisAlpha)
+                    .combineAlpha(MediumEmphasisAlpha)
+                BorderStroke(Dp.Hairline, borderColor)
+            },
+            onCheckedChange = {
+                updatedOnClick?.invoke()
+            },
+        ) {
+            if (leading != null) {
+                Box(
+                    modifier = Modifier
+                        .size(18.dp),
+                ) {
+                    leading()
+                }
+                Spacer(
+                    modifier = Modifier
+                        .width(8.dp),
                 )
             }
-        },
-        onClick = onClick,
-        enabled = enabled,
-    )
+            Column {
+                Text(
+                    title,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    color = LocalContentColor.current,
+                    style = MaterialTheme.typography.labelLarge,
+                )
+                val summaryOrNull = text?.takeUnless { it.isBlank() }
+                ExpandedIfNotEmpty(valueOrNull = summaryOrNull) { summary ->
+                    Text(
+                        summary,
+                        maxLines = 2,
+                        overflow = TextOverflow.Ellipsis,
+                        style = MaterialTheme.typography.labelSmall,
+                        color = LocalContentColor.current
+                            .combineAlpha(alpha = MediumEmphasisAlpha),
+                    )
+                }
+            }
+        }
+    }
 }
 
 @Composable

d0c5da61e5720273d74a7ce78940242ec8406bae
Artem Chepurnyi <mail@artemchep.com>
2025-12-14 09:38:49 +0200

    fix(Android): When verifying if a URI exists or not consider 0-bytes files as empty

--- common/src/androidMain/kotlin/com/artemchep/keyguard/copy/FileServiceAndroid.kt
+++ common/src/androidMain/kotlin/com/artemchep/keyguard/copy/FileServiceAndroid.kt
@@ -3,8 +3,7 @@ package com.artemchep.keyguard.copy
 import android.app.Application
 import android.content.Context
 import android.net.Uri
-import android.provider.BaseColumns
-import android.provider.DocumentsContract
+import android.provider.OpenableColumns
 import androidx.core.net.toFile
 import org.kodein.di.DirectDI
 import org.kodein.di.instance
@@ -31,23 +30,42 @@ class FileServiceAndroid(
                     .exists()
             }
 
-            else -> {
-                var stream: InputStream? = null
-                try {
-                    stream = readFromFile(uri)
-                    true // exists
-                } catch (_: Throwable) {
-                    false // doesn't exist
-                } finally {
-                    try {
-                        stream?.close()
-                    } catch (_: IOException) {
-                        // Ignored
-                    }
-                }
+            else -> exists(parsedUri)
+        }
+    }
+
+    private fun exists(uri: Uri): Boolean {
+        // When I create files using the document provider APIs it immediately
+        // creates a 0-bytes file before returning a URI.
+        runCatching {
+            val size = readFileSize(uri)
+            return size > 0L
+        }
+
+        var stream: InputStream? = null
+        return try {
+            stream = readFromFile(uri.toString())
+            true // exists
+        } catch (_: Throwable) {
+            false // doesn't exist
+        } finally {
+            try {
+                stream?.close()
+            } catch (_: IOException) {
+                // Ignored
             }
         }
     }
+
+    private fun readFileSize(uri: Uri): Long =
+        context.contentResolver.query(uri, null, null, null, null)
+            ?.use { cursor ->
+                if (!cursor.moveToFirst()) return@use null
+                // Get the size column
+                val sizeIndex = cursor.getColumnIndex(OpenableColumns.SIZE)
+                if (!cursor.isNull(sizeIndex)) cursor.getLong(sizeIndex) else -1L
+            }
+            ?: -1L
 
     override fun readFromFile(uri: String): InputStream {
         val parsedUri = uri.toUri()

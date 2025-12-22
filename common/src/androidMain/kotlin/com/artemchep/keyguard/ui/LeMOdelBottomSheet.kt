ad8db9703c687138d61b871dfbff8b5c5d24f942
Artem Chepurnyi <mail@artemchep.com>
2025-12-22 20:23:18 +0200

    fix(Android): Generator sheet doesn't have items' background

--- common/src/androidMain/kotlin/com/artemchep/keyguard/ui/LeMOdelBottomSheet.kt
+++ common/src/androidMain/kotlin/com/artemchep/keyguard/ui/LeMOdelBottomSheet.kt
@@ -5,8 +5,11 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.dp
+import com.artemchep.keyguard.ui.surface.LocalSurfaceElevation
+import com.artemchep.keyguard.ui.surface.SurfaceElevation
 
 @Composable
 actual fun LeMOdelBottomSheet(
@@ -35,8 +38,19 @@ actual fun LeMOdelBottomSheet(
                 onDismissRequest = onDismissRequest,
                 sheetState = bottomSheetState,
                 content = {
-                    val contentPadding = PaddingValues(0.dp)
-                    content(contentPadding)
+                    // Because the modal bottom sheet component provides its
+                    // own background color we have to hardcode the surface
+                    // elevation to provide consistent styling for the items.
+                    val surfaceElevation = SurfaceElevation(
+                        from = 0f,
+                        to = 0.5f,
+                    )
+                    CompositionLocalProvider(
+                        LocalSurfaceElevation provides surfaceElevation,
+                    ) {
+                        val contentPadding = PaddingValues(0.dp)
+                        content(contentPadding)
+                    }
                 },
             )
         }

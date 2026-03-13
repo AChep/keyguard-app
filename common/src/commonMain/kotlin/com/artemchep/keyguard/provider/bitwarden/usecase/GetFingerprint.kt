344f9abe30f9bd36775b5c665719622618685ce8
Artem Chepurnyi <mail@artemchep.com>
2026-03-13 17:55:38 +0200

    refactor: Rewrite GetFingerprint.kt to not use BigInteger

--- common/src/commonMain/kotlin/com/artemchep/keyguard/provider/bitwarden/usecase/GetFingerprint.kt
+++ common/src/commonMain/kotlin/com/artemchep/keyguard/provider/bitwarden/usecase/GetFingerprint.kt
@@ -11,7 +11,6 @@ import com.artemchep.keyguard.provider.bitwarden.usecase.util.pbk
 import kotlinx.coroutines.Dispatchers
 import org.kodein.di.DirectDI
 import org.kodein.di.instance
-import java.math.BigInteger
 import kotlin.coroutines.CoroutineContext
 import kotlin.math.ceil
 import kotlin.math.ln
@@ -54,40 +53,45 @@ class GetFingerprintImpl(
             wordList = wordList,
         ).joinToString("-")
     }
+}
 
-    private fun generateHashPhrase(
-        hash: ByteArray,
-        wordList: List<String>,
-        minimumEntropy: Int = 64,
-    ): List<String> {
-        val wordsCount = wordList.size
-        val wordsCountBi = BigInteger.valueOf(wordsCount.toLong())
+private fun generateHashPhrase(
+    hash: ByteArray,
+    wordList: List<String>,
+    minimumEntropy: Int = 64,
+): List<String> {
+    val wordsCount = wordList.size
 
-        val entropyPerWord = ln(wordsCount.toDouble()) / ln(2.0)
-        val entropyAvailable = hash.size * 4
-        var remainingWordsCount = ceil(minimumEntropy / entropyPerWord)
-        if (remainingWordsCount * entropyPerWord > entropyAvailable) {
-            throw IllegalStateException("Output entropy of hash function is too small")
-        }
-
-        val phrase = mutableListOf<String>()
-        var hashNumber = BigInteger.ZERO
-        hash.toList()
-            .asReversed()
-            .forEachIndexed { index, byte ->
-                var v = BigInteger.valueOf(byte.toUByte().toLong())
-                repeat(index) {
-                    v = v.times(BigInteger.valueOf(256L))
-                }
-                hashNumber = hashNumber.plus(v)
-            }
-        while (remainingWordsCount-- > 0) {
-            val remainder = hashNumber
-                .remainder(wordsCountBi)
-                .toInt()
-            hashNumber = hashNumber.divide(wordsCountBi)
-            phrase.add(wordList[remainder])
-        }
-        return phrase
+    val entropyPerWord = ln(wordsCount.toDouble()) / ln(2.0)
+    val entropyAvailable = hash.size * Byte.SIZE_BITS
+    val remainingWordsCount = ceil(minimumEntropy / entropyPerWord).toInt()
+    if (remainingWordsCount * entropyPerWord > entropyAvailable) {
+        throw IllegalStateException("Output entropy of hash function is too small")
     }
+
+    val phrase = ArrayList<String>(remainingWordsCount)
+    val hashDigits = hash
+        .map { it.toInt() and 0xFF }
+        .toIntArray()
+    repeat(remainingWordsCount) {
+        val remainder = divideUnsignedBigEndianNumberInPlace(
+            digits = hashDigits,
+            divisor = wordsCount,
+        )
+        phrase += wordList[remainder]
+    }
+    return phrase
+}
+
+private fun divideUnsignedBigEndianNumberInPlace(
+    digits: IntArray,
+    divisor: Int,
+): Int {
+    var remainder = 0
+    for (i in digits.indices) {
+        val dividend = remainder * 256 + digits[i]
+        digits[i] = dividend / divisor
+        remainder = dividend % divisor
+    }
+    return remainder
 }

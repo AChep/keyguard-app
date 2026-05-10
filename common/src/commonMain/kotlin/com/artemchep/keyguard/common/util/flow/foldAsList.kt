d0b31a9f5b026a1d3c460420b55f25acb00c5087
Artem Chepurnyi <mail@artemchep.com>
2026-05-10 08:10:50 +0300

    refactor: Simpler List<Flow> -> Flow<List> implementation

--- common/src/commonMain/kotlin/com/artemchep/keyguard/common/util/flow/foldAsList.kt
+++ common/src/commonMain/kotlin/com/artemchep/keyguard/common/util/flow/foldAsList.kt
@@ -1,12 +1,5 @@
 package com.artemchep.keyguard.common.util.flow
 
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.flowOf
 
-inline fun <reified T> List<Flow<T>>.foldAsList(): Flow<List<T>> =
-    if (isEmpty()) {
-        flowOf(emptyList())
-    } else {
-        combine(this) { array -> array.toList() }
-    }
+inline fun <reified T> List<Flow<T>>.foldAsList() = combineToList()
